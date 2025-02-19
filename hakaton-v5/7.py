9da5978e445e152d334432ec6ada50dcc9bff337
nikamacharash1 <nikolozmacharashvili331@gmail.com>
2025-02-19 21:47:29 +0400

    first commiT

--- hakaton-v5/7.py
+++
@@ -1,3 +0,0 @@
-# # შექმენით რაიმე string ტიპის ცვლადი და შეამოწმეთ palindrome'ა თუ არა.												
-# word = input("please enter a word")
-# word1 = N
--- level010/homework/index.py
+++ level010/homework/index.py
@@ -25,8 +25,10 @@
 # for i in range(0, 100, 5):
 #     print(i)
 
-for i in range(100, 1, -5):
-    print(i)
+# for i in range(100, 1, -5):
+#     print(i)
+    
+# kdmck
   
 
 
